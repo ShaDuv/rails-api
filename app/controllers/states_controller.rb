0a646c35f3b2f9a87917e4d425f7fea880d52f7a
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-23 09:46:08 -0700

    add all controlls for state

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,18 +1,42 @@
 class StatesController < ApplicationController
   def index
-  end
+      @states = State.all
+      json_response @states
+    end
 
-  def show
-  end
+    def show
+      @state = State.find params[:id]
+      json_response @state
+    end
 
-  def create
-  end
+    def create
+      @state = State.create! state_params
+      json_response @state, :created
+    end
 
-  def update
-  end
+    def update
+      @state = State.find params[:id]
+      if @state.update! state_params
+        render status: :ok, json: {
+          message: "State updated successfully!"
+        }
+      end
+    end
 
-  def destroy
-  end
+    def destroy
+      @state = State.find params[:id]
+      if @state.destroy!
+        render status: :ok, json: {
+          message: "State destroyed successfully!"
+        }
+      end
+    end
+
+    private
+
+    def state_params
+      params.permit :name, :city, :country
+    end
 
 
 end
