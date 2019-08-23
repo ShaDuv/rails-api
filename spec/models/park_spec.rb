515f06d9ec9e89a8b4eb0c081a8fe3f2a5883665
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-23 11:37:11 -0700

    passes parks shoulda matchers

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Park, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:state) }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :address }
+  it { should validate_presence_of :city }
 end
