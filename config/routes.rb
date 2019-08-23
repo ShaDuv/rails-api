20adc7ca83e3c1a3ac3f00e145e3ac29e0d22e0b
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-23 09:07:12 -0700

    config nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :parks
-  resources :states
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :states do
+    resources :parks
+  end
 end
