61d629b4b8dec0f7b609218a8377ac0a029aa8b9
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-23 11:24:22 -0700

    add shoulda matchers for etesting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot_rails'
+
+class Seed
+  def self.begin
+    State.destroy_all
+    seed = Seed.new
+    seed.generate_states_with_parks
+  end
+
+  def generate_states_with_parks
+    100.times do
+      FactoryBot.create(:state_with_parks)
+    end
+  end
+end
+
+Seed.begin
