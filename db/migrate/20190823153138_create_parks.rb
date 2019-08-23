ab8cf990682090db2ebeb25dcef6b299ee5d6269
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-23 08:50:31 -0700

    create database, state migration file and places migration files

--- db/migrate/20190823153138_create_parks.rb
+++ db/migrate/20190823153138_create_parks.rb
@@ -1,7 +1,10 @@
 class CreateParks < ActiveRecord::Migration[5.2]
   def change
     create_table :parks do |t|
-
+      t.string :name
+      t.string :address
+      t.string :city
+      t.belongs_to :state, foreign_key: true
       t.timestamps
     end
   end
