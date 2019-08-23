d738a657c9793b73277542470df2bd8735884cea
Shawna Duvall <shawnaduvall@gmail.com@>
2019-08-23 13:30:25 -0700

    passes 200 test

--- spec/requests/states_routes_spec.rb
+++ spec/requests/states_routes_spec.rb
@@ -9,4 +9,7 @@ describe 'get states route', type: :request do
   it 'returns all states' do
     expect(JSON.parse(response.body).size).to eq(20)
   end
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
 end
